65dbc59e9cedf657adfd13ce786710a04d950e19
[MaurisioSQA] <[mfagotto@gmail.com]>
2023-07-04 21:39:48 -0500

    ultimos cambios

--- app/src/main/java/com/example/behealthy/UserUpdateActivity.java
+++ app/src/main/java/com/example/behealthy/UserUpdateActivity.java
@@ -1,5 +1,6 @@
 package com.example.behealthy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,22 +9,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class UserUpdateActivity extends AppCompatActivity {
 
 
     Button updateProfileButton;
-    EditText updateNom, updateCon, updateUsu, updateCorreo, nombre;
-    String nom, clave, usu, correo;
-    DatabaseReference databaseReference;
+    EditText updateNom, updateCon, updateCorreo, usuario;
+
+    String key;
+
     StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,62 +41,100 @@ public class UserUpdateActivity extends AppCompatActivity {
         updateNom = (EditText) findViewById(R.id.update_name);
         updateCon = (EditText) findViewById(R.id.update_password);
         updateCorreo = (EditText) findViewById(R.id.update_email);
-        nombre = (EditText) findViewById(R.id.edtNom);
+        usuario = (EditText) findViewById(R.id.edtNom);
         updateProfileButton = (Button) findViewById(R.id.updateUser_Button);
+
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            Glide.with(UserUpdateActivity.this);
+            updateNom.setText(bundle.getString("name"));
+            updateCon.setText(bundle.getString("password"));
+            updateCorreo.setText(bundle.getString("email"));
+            key = bundle.getString("key");
+        }
+
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("users").child("key");
+
     }
 
-    public void checkIfUserExists(View view) {
-        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("users");
 
-        Query query = databaseRef.orderByChild("username").equalTo(nombre.getText().toString().trim());
+
+    public void checkIfUserExists(View view) {
+        String username  =  usuario.getText().toString().trim();
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("users");
+        Query query = databaseRef.orderByChild("username").equalTo(username);
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    // El registro con el nombre dado existe en la base de datos.
                     Toast.makeText(UserUpdateActivity.this, "Usuario Encontrado", Toast.LENGTH_LONG).show();
 
                 } else {
-                    // El registro con el nombre dado no existe en la base de datos.
                     Toast.makeText(UserUpdateActivity.this, "Usuario No Encontrado", Toast.LENGTH_LONG).show();
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                // Ocurrió un error al leer la base de datos.
-                // Puedes manejar el error aquí.
             }
         });
     }
 
-   public void updatePefil(View view) {
-        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("users");
 
-        Query query = databaseRef.orderByChild("username").equalTo(nombre.getText().toString().trim());
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+
+   public void updatePerfil(View view) {
+       String username  =  usuario.getText().toString().trim();
+       String name   = updateNom.getText().toString().trim();
+       String password= updateCon.getText().toString().trim();
+       String email = updateCorreo.getText().toString().trim() ;
+       DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("users").child("key");
+
+
+       storageReference = FirebaseStorage.getInstance().getReference().child("users").child(key);
+
+                   HelperClass helperClass = new HelperClass(name, email, username, password);
+
+                   databaseRef.setValue(helperClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                       @Override
+                       public void onComplete(@NonNull Task<Void> task) {
+                           if (task.isSuccessful()) {
+                               Toast.makeText(UserUpdateActivity.this, "Datos Actualizados", Toast.LENGTH_SHORT).show();
+                               finish();
+                           }
+                       }
+                   }).addOnFailureListener(new OnFailureListener() {
+                       @Override
+                       public void onFailure(@NonNull Exception e) {
+                           Toast.makeText(UserUpdateActivity.this, "No se actualizaron datos", Toast.LENGTH_SHORT).show();
+                       }
+                   });
+
+               }
+
+
+    public void updateData(View view) {
+        String username  =  usuario.getText().toString().trim();
+        String name   = updateNom.getText().toString().trim();
+        String password= updateCon.getText().toString().trim();
+        String email = updateCorreo.getText().toString().trim() ;
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("users").child(username);
+
+        HelperClass helperClass = new HelperClass(name, email, username,password);
+
+        databaseRef.setValue(helperClass).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    dataSnapshot.getRef().child("name").setValue(updateNom.getText().toString().trim());
-                    dataSnapshot.getRef().child("password").setValue(updateCon.getText().toString().trim());
-                    dataSnapshot.getRef().child("email").setValue(updateCorreo.getText().toString().trim());
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
 
-                    Toast.makeText(UserUpdateActivity.this, "Usuario Actualizado", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(UserUpdateActivity.this, "Usuario No Existe", Toast.LENGTH_LONG).show();
+                    Toast.makeText(UserUpdateActivity.this, "Datos Actualizados", Toast.LENGTH_SHORT).show();
                 }
             }
-
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Ocurrió un error al leer la base de datos.
-                Toast.makeText(UserUpdateActivity.this, "Error al acceder a la base de datos", Toast.LENGTH_LONG).show();
+            public void onFailure(@NonNull  Exception e) {
+                Toast.makeText(UserUpdateActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
+};
 
-
-
-
-}
