72778f6d2359deb0de08f834b51206696c9549b1
Stefano <stefanop21@outlook.es>
2023-07-04 21:42:12 -0500

    navigation bar

--- app/src/main/java/com/example/behealthy/HomeActivity.java
+++ app/src/main/java/com/example/behealthy/HomeActivity.java
@@ -3,12 +3,17 @@ package com.example.behealthy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,6 +27,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, profile;
     FloatingActionButton fab;
     RecyclerView recyclerView;
     List<DataClass> dataList;
@@ -35,10 +43,33 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        drawerLayout=findViewById(R.id.drawerlayout);
+        menu=findViewById(R.id.menu);
+        home=findViewById(R.id.principal);
+        profile=findViewById(R.id.profile);
         fab = findViewById(R.id.fab);
         recyclerView = findViewById(R.id.activities_view);
         searchView = findViewById(R.id.search_view);
         searchView.clearFocus();
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawer(drawerLayout);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(HomeActivity.this, ProfileActivity.class);
+            }
+        });
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(HomeActivity.this, 1);
         recyclerView.setLayoutManager(gridLayoutManager);
@@ -96,6 +127,27 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public static void  openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent=new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 
     public void searchList(String text) {
